3cb1065ee0011e3ee159d0d3c08aa1a383ae8491
Bernard0Pires <bernardopires302@gmail.com>
2022-04-26 19:37:28 +0100

    comentado

--- program.py
+++ program.py
@@ -5,3 +5,16 @@ sys.stdout.reconfigure(encoding="utf-8")
 
 if __name__ == "__main__":
     cli.main()
+
+# Programa que permite o armazenamento, consulta e modificação de uma lista ligada (neste caso, contendo nomes de países.)
+# Este programa permite:
+#   Registar país no início da lista (RPI)
+#   Registar país no fim da lista (RPF)
+#   Registar país depois de outro elemento (RPDE)
+#   Registar país antes de outro elemento (RPAE)
+#   Registar país num determinado índice (RPII)
+#   Verificar número de elementos (VNE)
+#   Verificar se um país se encontra a lista (VP)
+#   Eliminar i 1º elemento (EPE)
+#   Eliminar o último elemento (EUE)
+#   Eliminar um determinado país (EP)
--- views/cli.py
+++ views/cli.py
@@ -39,11 +39,11 @@ def main():
             
             if user_comand[0] == "VNE":
                 number_of_elements:int = country_list.get_count()
-                print(f"O número de elementos são {number_of_elements}")
+                print(f"O número de elementos são {number_of_elements}.")
             
             if user_comand[0] == "VP":
                 country_to_search:str = user_comand[1]
-                country_to_search_error:bool = country_list.search_item(country_to_search)
+                country_to_search_error:bool = country_list.search_item(country_to_search)  #Variável de controlo que retorna se o país se encontra na lista(True) ou não (False).
 
                 if country_to_search_error == True:
                     print(f"O país {country_to_search} encontra-se na lista.")
@@ -52,18 +52,18 @@ def main():
                     print(f"O país {country_to_search} não se encontra na lista.")
         
             if user_comand[0] == "EPE":
-                country_to_eliminate:str = country_list.start_node.get_item()
+                country_to_eliminate:str = country_list.start_node.get_item()  #Variável que obtém o primeiro elemento da lista para, após a sua eliminação, dar print do elemento eliminado.
                 country_list.delete_at_start()
                 print(f"O país {country_to_eliminate} foi eliminado da lista.")
 
             if user_comand[0] == "EUE":
-                country_to_eliminate:str = country_list.get_last_node()
+                country_to_eliminate:str = country_list.get_last_node()     #Variável que obtém o último elemento da lista para, após a sua eliminação, dar print do elemento eliminado.
                 country_list.delete_at_end()
                 print(f"O país {country_to_eliminate} foi eliminado da lista.")
 
             if user_comand[0] == "EP":
                 country_to_eliminate:str = user_comand[1]
-                check_country_in_list:bool = country_list.search_item(country_to_eliminate)
+                check_country_in_list:bool = country_list.search_item(country_to_eliminate) #Variável de controlo que retorna True, caso o país se encontre na lista e, de seguida, é eliminado.
                 
                 if check_country_in_list == False:
                     print(f"O país {country_to_eliminate} não se encontra na lista.")

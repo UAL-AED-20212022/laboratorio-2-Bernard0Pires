aa904e18bdbcdf9045db8d6f3ae0ac91309e0e10
Bernard0Pires <bernardopires302@gmail.com>
2022-04-26 18:38:31 +0100

    1st round of tests

--- views/cli.py
+++ views/cli.py
@@ -1,48 +1,47 @@
 from re import A
 from models.LinkedList import LinkedList
-from models.Node import Node
 
 
 def main():
 
     country_list = LinkedList()
     while True:
-        user_comand:list[str] = input().lower().split(" ")   
+        user_comand:list[str] = input().split(" ")   
 
-        if user_comand[0] in ["rpi", "rpf", "rpde", "rpae", "rpii", "vne", "vp", "epe", "eue", "ep"]:
-            if user_comand[0] == "rpi":
+        if user_comand[0] in ["RPI", "RPF", "RPDE", "RPAE", "RPII", "VNE", "VP", "EPE", "EUE", "EP"]:
+            if user_comand[0] == "RPI":
                 country_to_add:str = user_comand[1]
                 country_list.insert_at_start(country_to_add)
                 country_list.traverse_list()
 
-            if user_comand[0] == "rpf":
+            if user_comand[0] == "RPF":
                 country_to_add:str = user_comand[1]
                 country_list.insert_at_end(country_to_add)
                 country_list.traverse_list()
             
-            if user_comand[0] == "rpde":
+            if user_comand[0] == "RPDE":
                 country_to_add:str = user_comand[1]
                 reference_country:str = user_comand[2]
                 country_list.insert_after_item(reference_country, country_to_add)
                 country_list.traverse_list()
 
-            if user_comand[0] == "rpae":
+            if user_comand[0] == "RPAE":
                 country_to_add:str = user_comand[1]
                 reference_country:str = user_comand[2]
                 country_list.insert_before_item(reference_country, country_to_add)
                 country_list.traverse_list()
 
-            if user_comand[0] == "rpii":
+            if user_comand[0] == "RPII":
                 country_to_add:str = user_comand[1]
                 desired_index:int = int(user_comand[2])
                 country_list.insert_at_index(desired_index, country_to_add)
                 country_list.traverse_list()
             
-            if user_comand[0] == "vne":
+            if user_comand[0] == "VNE":
                 number_of_elements:int = country_list.get_count()
                 print(f"O número de elementos são {number_of_elements}")
             
-            if user_comand[0] == "vp":
+            if user_comand[0] == "VP":
                 country_to_search:str = user_comand[1]
                 country_to_search_error:bool = country_list.search_item(country_to_search)
 
@@ -52,17 +51,17 @@ def main():
                 if country_to_search_error == False:
                     print(f"O país {country_to_search} não se encontra na lista.")
         
-            if user_comand[0] == "epe":
+            if user_comand[0] == "EPE":
                 country_to_eliminate:str = country_list.start_node.get_item()
                 country_list.delete_at_start()
                 print(f"O país {country_to_eliminate} foi eliminado da lista.")
 
-            if user_comand[0] == "eue":
+            if user_comand[0] == "EUE":
                 country_to_eliminate:str = country_list.get_last_node()
                 country_list.delete_at_end()
                 print(f"O país {country_to_eliminate} foi eliminado da lista.")
 
-            if user_comand[0] == "ep":
+            if user_comand[0] == "EP":
                 country_to_eliminate:str = user_comand[1]
                 check_country_in_list:bool = country_list.search_item(country_to_eliminate)
                 
